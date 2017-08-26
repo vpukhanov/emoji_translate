9f27e2c524bd4041cc0fb3341a50e2e6bcf0a80c
Vyacheslav Pukhanov <vyacheslav.pukhanov@gmail.com>
2017-08-26 22:49:41 +0300

    Adds rspec tests

--- spec/emoji_translate_spec.rb
+++ spec/emoji_translate_spec.rb
@@ -5,7 +5,33 @@ RSpec.describe EmojiTranslate do
     expect(EmojiTranslate::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "inserts matching emojis into sentences" do
+  	sentence = "the house is on fire and the cat is eating the cake";
+  	translated = EmojiTranslate.translate sentence
+
+  	expect(translated).to include("🔥")
+  	expect(translated).to include("🐱")
+  	expect(translated).to include("🍰")
+  end
+
+  it "inserts matching keyword emojis into sentences" do
+    sentence = "I ate a donut in USA"
+    translated = EmojiTranslate.translate sentence
+
+    expect(translated).to include("🍩")
+  end
+
+  it "saves punctuation" do
+  	sentence = "Hello, world!"
+  	translated = EmojiTranslate.translate sentence
+
+  	expect(translated).to eq("👋, 🌎!")
+  end
+
+  it "saves line breaks" do
+  	sentence = "So\nhere\nwe\nare\nagain!"
+  	translated = EmojiTranslate.translate sentence
+
+  	expect(translated.lines.length).to eq(5)
   end
 end
